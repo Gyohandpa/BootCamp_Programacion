5b426286c0063aa91e2ff83bb7b8d441b89d5e16
Gyohandpa <gyohand.perdomo@udea.edu.co>
2025-07-25 12:13:23 -0500

    Inicio JavaScript

---
+++ BootCamp/BootCamp_dia_5/reto 8 pre 1/js/scripts.js
@@ -0,0 +1,13 @@
+// Obtener la referencia de los documentos de la pagina
+// funcion(parametro1,parametro2,...si esta vacia no tiene requerimientos){constante aclarada con la funcion document.getElementBy Id}
+document.addEventListener('DOMContentLoaded', function(){
+    const inputBox = document.getElementById('inputBox');
+    const showBtn = document.getElementById('showBtn');
+    const outputBox =document.getElementById('outputBox');
+    //Añadir un observador al boton.
+    showBtn.addEventListener('click', function(){
+        const message = inputBox.value;
+        /* console.log(message) */
+        console.log('Dato Ingresado')
+    })
+})
