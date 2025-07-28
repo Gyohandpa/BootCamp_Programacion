3a8c5e24b795ef7a49f810e683817e21797888b5
Gyohandpa <gyohand.perdomo@udea.edu.co>
2025-07-28 12:33:21 -0500

    reto_8_pre3_solucion

--- BootCamp/BootCamp_dia_6/reto_8_pre2/js/scripts.js
+++ BootCamp/BootCamp_dia_6/reto_8_pre2/js/scripts.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const outputBox =document.getElementById('outputBox');
 
     inputBox.addEventListener('input', function(){
-       const message = inputBox.value;
-       outputBox.textContent = message; 
+       const message = inputBox.value.toUpperCase();
+       /* const message = inputBox.value.toUpperCase(); Convierte todo en Mayuscula*/
+       outputBox.textContent =message; 
     });
 })
